6342c2dab03db359754ef35816fca71316a6e886
puterakahfi <puterakahfi@gmail.com>
2016-04-14 22:14:38 +0700

    updated gulpfile.js remove unused tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,99 +9,16 @@ var gulp = require('gulp'),
 	var inject = require('gulp-inject');
 	var browsersync = require('browser-sync');
     var changed = require('gulp-changed');
-	gulp.task('connect', function() {
-		connect.server();
-	});
 
-
+	/**
+	* START SERVER
+	*/
 	gulp.task('webserver', function() {
-		gulp.src('app')
+		gulp.src('./')
 		.pipe(webserver({
 			livereload: true,
 			fallback: 'index.html',
-			directoryListing: true,
-			open: true
 		}));
 	});
-
-/**
- *  * Run the build task and start a server with BrowserSync
- *   */
-	gulp.task('browsersync', function() {
-		browsersync(config);
-	});
-
-	gulp.task('vendorcss', function(){
-		return gulp
-		// set source
-		.src(config.paths.vendorcss)
-		//      // write to vendor.min.css
-		.pipe(concat('vendor.min.css'))
-		//              // write to dest
-		.pipe(minifyCss())
-		.pipe(gulp.dest(config.paths.dest));
-	});
-
-	gulp.task('customcss', function(){
-		return gulp
-		// set source
-		.src(config.paths.customcss)
-		//      // write to vendor.min.css
-		.pipe(concat('custom.min.css'))
-		//              // write to dest
-		.pipe(minifyCss())
-		.pipe(gulp.dest(config.paths.dest));
-	});
-
-
-	gulp.task('vendorjs', function(){
-		return gulp
-		// set source
-		.src(config.paths.vendorjs)
-		//              // write to vendor.min.js
-		.pipe(concat('vendor.min.js'))
-		//                              // start tracking size
-		//.pipe(bytediff.start())
-		//                                              // uglify js
-		.pipe(uglify())
-		//                                                             // stop tracking size and output it using bytediffFormatter
-		//.pipe(bytediff.stop(bytediffFormatter))
-
-		// write to dest
-		.pipe(gulp.dest(config.paths.dest));
-	});
-
-
-	gulp.task('customjs', function(){
-		return gulp
-		// set source
-		.src(config.paths.customJs)
-		//              // write to vendor.min.js
-		.pipe(concat('custom.min.js'))
-		//                              // start tracking size
-		//.pipe(bytediff.start())
-		//                                              // uglify js
-		.pipe(uglify())
-		//                                                             // stop tracking size and output it using bytediffFormatter
-		//.pipe(bytediff.stop(bytediffFormatter))
-
-		// write to dest
-		.pipe(gulp.dest(config.paths.dest));
-	});
-
-
-	gulp.task('images',function(){
- return gulp.src(config.paths.images.src)
-     .pipe(changed(config.paths.images.dest)) // Ignore unchanged files
-	     .pipe(gulp.dest(config.paths.images.dest));
-});
-
-/**
- *  * Start browsersync task and then watch files for changes
- *   */
-	gulp.task('watch', ['browsersync'], function() {
-
-	});
-
 
 	gulp.task('default', ['connect']);
