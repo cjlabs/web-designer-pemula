f6af82cfe7c57f2ef82b1d7ee1de675f1c947dcd
puterakahfi <puterakahfi@gmail.com>
2016-04-16 14:55:54 +0700

    updated gulp and package

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 var gulp = require('gulp'),
-	webserver = require('gulp-webserver')
-	concat = require('gulp-concat')
-	sass = require('gulp-sass');
-	var usemin = require('gulp-usemin');
-	var uglify = require('gulp-uglify');
-	var minifyCss = require('gulp-clean-css');
-	var config = require('./gulp.config.js');
-	var inject = require('gulp-inject');
-	var browsersync = require('browser-sync');
-    var changed = require('gulp-changed');
+requireDir = require('require-dir')
+webserver = require('gulp-webserver')
+concat = require('gulp-concat')
+sass = require('gulp-sass');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-clean-css');
+var config = require('./gulp.config.js');
+var inject = require('gulp-inject');
+var browsersync = require('browser-sync');
+var changed = require('gulp-changed');
+
+var requireDir = require('require-dir');
+
+// Require all tasks in gulp/tasks, including subfolders
+requireDir('./gulp/tasks', { recurse: true });
 
 	/**
 	* START SERVER
@@ -21,10 +27,9 @@ var gulp = require('gulp'),
 		}));
 	});
 
-
 	gulp.task('copyfonts', function() {
 		   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
-		      .pipe(gulp.dest('./dist/fonts'));
+	      .pipe(gulp.dest('./dist/fonts'));
 	});
-
-	gulp.task('default', ['connect']);
+	
+	
