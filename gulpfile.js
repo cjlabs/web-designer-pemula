d9b92b90eb38b4de59633a478ac7bcce08504f17
puterakahfi <puterakahfi@gmail.com>
2016-04-16 15:19:45 +0700

    move task web server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp'),
 requireDir = require('require-dir')
-webserver = require('gulp-webserver')
 concat = require('gulp-concat')
 sass = require('gulp-sass');
 var usemin = require('gulp-usemin');
@@ -16,16 +15,6 @@ var requireDir = require('require-dir');
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./gulp/tasks', { recurse: true });
 
-	/**
-	* START SERVER
-	*/
-	gulp.task('webserver', function() {
-		gulp.src('./')
-		.pipe(webserver({
-			livereload: true,
-			fallback: 'index.html',
-		}));
-	});
 
 	gulp.task('copyfonts', function() {
 		   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
