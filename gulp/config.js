8e3aa23009cbe4e925f73e67d30fdb8ee7767436
puterakahfi <puterakahfi@gmail.com>
2016-04-17 10:49:26 +0700

    added gulp config

---
+++ gulp/config.js
@@ -0,0 +1,36 @@
+var src               = 'app';
+var build             = 'build';
+var development       = 'build/development';
+var production        = 'build/production';
+var srcAssets         = 'src';
+var developmentAssets = 'build/assets';
+var productionAssets  = 'build/production/assets';
+
+module.exports = {
+	browsersync: {
+		development: {
+			server: {
+				baseDir: [development, build, src]
+			},
+			port: 9999,
+			files: [
+				developmentAssets + '/css/*.css',
+				developmentAssets + '/js/*.js',
+				developmentAssets + '/images/**',
+				developmentAssets + '/fonts/*'
+			]
+		}
+	},
+	images: {
+		src:  srcAssets + '/images/**/*',
+		dest: developmentAssets + '/images'
+	},
+	watch: {
+		sass:    srcAssets + '/scss/**/*.{sass,scss}',
+		css: srcAssets + '/css/**/*.css',
+		scripts: srcAssets + '/js/**/*.js',
+		images:  srcAssets + '/img/**/*',
+		sprites: srcAssets + '/img/**/*.png',
+		svg:     'vectors/*.svg'
+	}
+};
