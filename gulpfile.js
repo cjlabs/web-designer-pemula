7e13c5cffa327a32d5491b21a0756ade0aaa0b5d
puterakahfi <puterakahfi@gmail.com>
2016-04-17 10:49:36 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,7 @@ sass = require('gulp-sass');
 var usemin = require('gulp-usemin');
 var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-clean-css');
-var config = require('./gulp.config.js');
 var inject = require('gulp-inject');
-var browsersync = require('browser-sync');
 var changed = require('gulp-changed');
 
 var requireDir = require('require-dir');
@@ -16,9 +14,9 @@ var requireDir = require('require-dir');
 requireDir('./gulp/tasks', { recurse: true });
 
 
-	gulp.task('copyfonts', function() {
-		   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
-	      .pipe(gulp.dest('./dist/fonts'));
-	});
+gulp.task('copyfonts', function() {
+	   gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
+      .pipe(gulp.dest('./dist/fonts'));
+});
 	
 	
