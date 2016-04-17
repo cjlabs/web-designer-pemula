adefa411390dd33584e876ff1f25ed34a6d7b0d5
puterakahfi <puterakahfi@gmail.com>
2016-04-17 10:48:47 +0700

    added development tasks

--- gulp/tasks/development/watch.js
+++ gulp/tasks/development/watch.js
@@ -1,8 +1,15 @@
-var gulp =require('gulp');
+var gulp = require('gulp');
+var config = require('../../config').watch;
 
 /**
  *  * Start browsersync task and then watch files for changes
  *   */
 gulp.task('watch', ['browsersync'], function() {
-
-});
+	gulp.watch(config.css ['scripts', 'jshint']);
+/*
+gulp.watch(config.scripts, ['scripts', 'jshint']);
+	gulp.watch(config.images,  ['images']);
+	gulp.watch(config.svg,     ['copy:fonts']);
+	gulp.watch(config.sprites, ['sprites']);
+*/
+	});
