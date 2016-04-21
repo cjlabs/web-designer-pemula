207732431d7c7974bf78dd123ee4dcc9c38c8bb0
puterakahfi <puterakahfi@gmail.com>
2016-04-21 22:00:09 +0700

    update copy task'

--- gulp/tasks/development/copy.js
+++ gulp/tasks/development/copy.js
@@ -1,15 +1,29 @@
+/**
+* copy.js is for copy all assets to src directory
+* include css, jss, fonts
+* execute copy:vendor for todo all copy tasks
+* */
+
 var gulp  = require('gulp');
-var config = require('../../config.js').fonts;
+var font  = require('../../config.js').fonts;
 var vendors = require('../../config.js').vendors;
+var path = require('../../config.js').paths;
 
 gulp.task('copy:fonts', function() {
-	gulp.src(config.files)
-	.pipe(gulp.dest('./dist/fonts'));
+	gulp.src(font.files)
+	.pipe(gulp.dest(path.src + '/fonts/'));
+});
+
+gulp.task('copy:css', function(){
+
+});
+
+gulp.task('copy:js', function(){
+
 });
 
 
 gulp.task('copy:vendors', function(){
 	gulp.src(vendors.files)
-	.pipe(gulp.dest('./src/vendor'));
-
+	.pipe(gulp.dest('./src/vendor'));   
 });
